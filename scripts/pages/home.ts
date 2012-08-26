b378b888e2d1a6fefb51604717f6467c4304dba4
Sean Jezewski <seanwjezewski@gmail.com>
2012-08-26 02:49:01 -0700

    Fix login for mobile user agents

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,1 +1,14 @@
 # Place holder/example file
+
+$login_url = "https://www.dropbox.com/login"
+$login_url {
+  rewrite("link") 
+}
+
+$landing_url = "https://www.dropbox.com/home"
+$login_url {
+  rewrite("link")
+}
+
+# Redirect to login page
+export("Location", $login_url + "?cont=" + $landing_url)
