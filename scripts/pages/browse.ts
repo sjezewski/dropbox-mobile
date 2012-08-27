32d35623730239d1ce9722e149ac6ee820f345c7
Cagri Ozertem <cagri.ozertem@moovweb.com>
2012-08-26 23:23:45 -0700

    styling browse

--- scripts/pages/browse.ts
+++ scripts/pages/browse.ts
@@ -1,3 +1,8 @@
 $("/html/body") {
   add_class("mw_browse")
+
+  # remove the image that is displayed when the folder is empty
+  remove (".//div[@id='browse-empty']/img")
+
+  # insert_bottom("div", class: "mw_mask_dropdown", onclick: "x$('.mw_mask_dropdown').toggleClass('mw_mask_active');x$('#breadcrumb-dropdown').toggleClass('mw_active');")
 }
