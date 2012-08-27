d760e85895535976ceb28e682b3b13c1f3f7d8a1
Cagri Ozertem <cagri.ozertem@moovweb.com>
2012-08-27 14:42:35 -0700

    fixed the display problems create a dropbox account button shows up

--- scripts/pages/file_preview.ts
+++ scripts/pages/file_preview.ts
@@ -11,6 +11,14 @@ $("/html/body") {
       }
     }
   }
+  $(".//a[@id='create-an-account-button']") {
+    wrap ("div", class: "mw_create_acc") {
+      move_to("./ancestor::div[@class='nav-header']", "after")
+      $("./following-sibling::div[@id='shmodel-content-area']//div[@id='crocodoc-preview']") {
+        add_class("mw_top_changed_2")
+      }
+    }
+  }
 
 
   # Long URL:
@@ -24,21 +32,21 @@ $("/html/body") {
           $1 {
             replace("\\x3a",":")
             replace("\\x2f", "/")
-            replace("\\x2e", ".")            
+            replace("\\x2e", ".")
           }
           $link = $1
         }
       }
     }
   }
-  
+
   $(".//div[@id='modal-content']") {
     insert_before("div", id: "mw_link_prompt_container") {
       insert("div", id: "mw_link_prompt") {
         insert("label", for: "mw_share_link") {
           text("Copy Link:")
         }
-        insert("input", id: "mw_share_link", type: "text", value: $link) 
+        insert("input", id: "mw_share_link", type: "text", value: $link)
       }
     }
   }
