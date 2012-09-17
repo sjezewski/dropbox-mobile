3264ec77c4f7764adfce7c10fee9c19dd53b3b35
Sean Jezewski <sean@moovweb.com>
2012-09-17 12:33:17 -0700

    Repair another ajax content type

--- scripts/content_type.ts
+++ scripts/content_type.ts
@@ -2,7 +2,7 @@ $content_type
 
 # Repair content-types by path. They don't set the content-type properly
 
-match($path, /^\/search\/warmup|^\/jse|^\/fb\/post|^\/web_timing_log|^\/add_sprites_used|^\/shmlog/) {
+match($path, /^\/search\/warmup|^\/jse|^\/fb\/post|^\/web_timing_log|^\/add_sprites_used|^\/shmlog|^\/google\/have_user/) {
 	$content_type = "text/plain"
 }
 
